c483c9aa2797af9a561cf9ed9538f81d35d34d32
Steven Kao <steven@skand.io>
2021-01-08 14:02:12 +1100

    initial commit

---
+++ src/ProductList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const APIURL = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/1';
+
+export default class ProductList extends Component {  
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
