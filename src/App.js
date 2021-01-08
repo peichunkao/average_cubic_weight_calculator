105565ac6a5e2fa49ed2bf9340146c571bbf28e6
Steven Kao <steven@skand.io>
2021-01-08 21:41:54 +1100

    added ProductList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import ProductList from './ProductList'
 
-
 function App() {
   return (
     <div className="App">
--- src/ProductList.js
+++ src/ProductList.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react'
 
 const APIURL = 'http://wp8m3he1wt.s3-website-ap-southeast-2.amazonaws.com/api/products/1';
+export default class ProductList extends Component {
+    constructor(props) {
+		super(props)
+		this.state = {
+			products:[]
+		}
+    }
+    
+    componentDidMount() {
+        this.loadProducts();
+	}
 
-export default class ProductList extends Component {  
+    loadProducts() {
+        fetch(APIURL)
+		.then(res => {
+			if(!res.ok) {
+				if(res.status >= 400 || res.status < 500) {
+					const data = res.json();
+					let errorMessage = { errorMessage: data.message };
+					throw errorMessage;
+				} else {
+					let errorMessage = {errorMessage: 'Please try again later, server is not responding.'}
+					throw errorMessage
+				}
+			} else {
+				return res.json()
+			}
+        })
+        .then(res => this.setState({products: res.objects}))
+	}
+
     render() {
+        const { products } = this.state;
+        const { category } = this.props;
+
+        const filteredProducts = products.filter(product => product.category === category)
+        console.log(filteredProducts)
+
         return (
             <div>
-                
+                <h1>Product List</h1>
             </div>
         )
     }
